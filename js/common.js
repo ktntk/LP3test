aa618cee40884ba02fdf95fe7dae1dd2b15e4b98
taku.kitano <kitaku95026@gmail.com>
2022-07-21 02:42:08 +0900

    レスポンシブ設定前

--- js/common.js
+++ js/common.js
@@ -1,37 +1,20 @@
 // // ローディングアニメーション
-// const loadAnime = $('.loader-text');
-// $(window).on('load', function () {
-//   loadAnime.delay(1200).fadeOut(1000);
-// });
-// // スライド
-// $(window).on('load', function () {
-//   $('.loader-slide1').addClass('open1');
-// });
-// $(window).on('load', function () {
-//   $('.loader-slide2').addClass('open2');
-// });
-
-
-//スクロールすると上部に固定させるための設定を関数でまとめる
-function FixedAnime() {
-  var headerH = $('#header').outerHeight(true);
-  var scroll = $(window).scrollTop();
-  if (scroll >= headerH) {//headerの高さ以上になったら
-    $('#header').addClass('fixed');//fixedというクラス名を付与
-  } else {//それ以外は
-    $('#header').removeClass('fixed');//fixedというクラス名を除去
-  }
-}
-
-//ナビゲーションをクリックした際のスムーススクロール
-$('nav a').click(function () {
-  var elmHash = $(this).attr('href'); //hrefの内容を取得
-  var pos = Math.round($(elmHash).offset().top - 120);  //headerの高さを引く
-  $('body,html').animate({ scrollTop: pos }, 500);//取得した位置にスクロール※数値が大きいほどゆっくりスクロール
-  return false;//リンクの無効化
+$(window).on('load', function () {
+  $("#loading-logo").delay(1500).fadeOut('slow');
+  $("#loading").delay(1500).fadeOut('slow', function () {
+    $('body').addClass('appear');
+  });
+  $('.loading_bg').on('animationend', function () {
+  });
 });
 
-
+//ナビゲーションスクロール
+$('nav a').click(function () {
+  var elmHash = $(this).attr('href');
+  var pos = Math.round($(elmHash).offset().top - 120);
+  $('body,html').animate({ scrollTop: pos }, 500);
+  return false;
+});
 
 // 回転
 ScrollReveal().reveal('.rotate', {
@@ -47,18 +30,41 @@ ScrollReveal().reveal('.up', {
   duration: 1200,
   reset: true
 });
+$(window).scroll(function () {
+  up();
+});
 
 // FAQ
 $('.question').on('click', function () {
   $('.answer').slideUp(500);
-
   var findElm = $(this).next(".answer");
-
   if ($(this).hasClass('close')) {
     $(this).removeClass('close');
     } else {
     $('.close').removeClass('close');
     $(this).addClass('close');
     $(findElm).slideDown(500);
+  }
+});
+
+// ハンバーガーメニュー
+window.onunload = function () { };
+function sizecheck() {
+  w = window.innerWidth ? window.innerWidth : $(window).width();
+  h = window.innerHeight ? window.innerHeight : $(window).height();
+}
+
+sizecheck();
+$(function () {
+  if (w > 769) {
+  } else {
+    $("header .drawer").click(function () {
+      $('body').toggleClass('nav-open');
+      $('header .header_nav').fadeToggle(200);
+    });
+    $("header li a").click(function () {
+      $('body').toggleClass('nav-open');
+      $('header .header_nav').fadeToggle(200);
+    });
   }
 });
